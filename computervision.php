081759efb65ab92c24abd00a6008940b5094e905
Rachel Gultom <57515563+RachelGultom2000@users.noreply.github.com>
2020-01-16 16:06:51 +0700

    computervision.html

--- computervision.php
+++ computervision.php
@@ -24,7 +24,7 @@
         // If you use a free trial subscription key, you shouldn't need to change
         // this region.
         var uriBase =
-            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+            "https://rachelvision.cognitiveservices.azure.com/";
  
         // Request parameters.
         var params = {
