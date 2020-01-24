7d7367352fd89a96fef45b3bcaefbc7912d29927
Rachel Gultom <57515563+RachelGultom2000@users.noreply.github.com>
2020-01-24 15:07:21 +0700

    Update computervision.php

--- computervision.php
+++ computervision.php
@@ -1,206 +1,105 @@
 <?php
-    require_once 'vendor/autoload.php';
-    require_once "./random_string.php";
-    
-    use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-    use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
-    use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
-    use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
-    use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;    
 
-      # Setup a specific instance of an Azure::Storage::Client
-      $connectionString = "DefaultEndpointsProtocol=https;AccountName=rachelwebapp;AccountKey=lfT4XdHsUBZQQWk/GS+Kb1jmQ6Yr9JRELXe2uU6RR6IV5M6wsTetKoHmYkyxpQIgJ3BC+HJ7Ea/HRCzP84uwaQ==;EndpointSuffix=core.windows.net";
-    
-      // Create blob client.
-      $blobClient = BlobRestProxy::createBlobService($connectionString);
-      
-      # Create the BlobService that represents the Blob service for the storage account
-      $createContainerOptions = new CreateContainerOptions();
-      
-      $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-      
-      // Set container metadata.
-      $createContainerOptions->addMetaData("key1", "value1");
-      $createContainerOptions->addMetaData("key2", "value2");
-  
-      $containerName = "blockblobs".generateRandomString();
-  
-      try{
-          // Create container.
-          $blobClient->createContainer($containerName, $createContainerOptions);
+if (isset($_POST['submit'])) {
+	if (isset($_POST['url'])) {
+		$url = $_POST['url'];
+	} else {
+		header("Location: index.php");
+	}
+} else {
+	header("Location: index.php");
+}
 
-        } catch(ServiceException $e){
-            // Handle exception based on error codes and messages.
-            // Error codes and messages are here:
-            // http://msdn.microsoft.com/library/azure/dd179439.aspx
-            $code = $e->getCode();
-            $error_message = $e->getMessage();
-            echo $code.": ".$error_message."<br />";
-        } catch(InvalidArgumentTypeException $e){
-            // Handle exception based on error codes and messages.
-            // Error codes and messages are here:
-            // http://msdn.microsoft.com/library/azure/dd179439.aspx
-            $code = $e->getCode();
-            $error_message = $e->getMessage();
-            echo $code.": ".$error_message."<br />";
-    }
-    if (isset($_POST['submit'])) {
-        
-            $temp = explode(".", $_FILES["images"]["name"]);
-            $fileToUpload =  $containerName.'.' . end($temp);
-            echo '<pre>';
-            
-        if (move_uploaded_file($_FILES['images']['tmp_name'], $fileToUpload)) {
-            
-            echo "File is valid, and was successfully uploaded.\n";
-
-            $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
-            fclose($myfile);
-
-            # Upload file as a block blob
-            echo "Uploading BlockBlob: ".PHP_EOL;
-            echo $fileToUpload;
-            
-            $content = fopen($fileToUpload, "r");
-
-            //Upload blob
-            $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-            header('location: computervision.php?containerName='.$containerName);
-
-        }
-    }
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sub 2 Azure</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<title>MACD Submission 2!</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
-      <div class="container">
-        <p class="lead">Upload sebuah gambar!</p>
-        <a href="index.php">Kembali</a>
-      </div>
-    <div class="container container-fluid">
-        <form action="" method="POST" enctype="multipart/form-data">
-            <div class="row">
-                <div class="col">
-                    <div class="card p-3 mb-3" style="width: 18rem;">
-                        <div class="custom-file mb-3">
-                            <input type="file" class="custom-file-input" id="customFile" name="images">
-                            <label class="custom-file-label" for="customFile">Choose file</label>
-                        </div>
-                        <input type="submit" name="submit" value="Analisis" class="btn btn-primary form-control">
-                    </div>
+	<script type="text/javascript">
+		$(document).ready(function () {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
 
-                    <?php
-                     
+        // Replace <Subscription Key> with your valid subscription key.
+        var subscriptionKey = "4c89c2f802374e608cc33c0c2e534972";
 
-                        if(!empty($_GET['containerName'])){
-                            $containerName = $_GET['containerName'];
+        // You must use the same Azure region in your REST API method as you used to
+        // get your subscription keys. For example, if you got your subscription keys
+        // from the West US region, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the "westus" region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        var uriBase =
+        "https://rachelvision.cognitiveservices.azure.com/";
 
-                            $listBlobsOptions = new ListBlobsOptions();
-                        
-                            
-                            do{
-                                $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-                               
-                                foreach ($result->getBlobs() as $blob)
-                                {
-                                
-                                    echo '<img src='.$blob->getUrl().' id="images">';
-                                }
-                                
-                                $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-                            } while($result->getContinuationToken());
-                        }
-                    ?>
-                </div>
-            </div>
-            <div class="row" id="result">
-                <div class="col-md-12">
-                      <h5 id="captions"></h5>
-                </div>
-            </div>
-        </form>
-    </div>
-</body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script> 
-<script>
-    $( "#result" ).hide();
+        // Request parameters.
+        var params = {
+        	"visualFeatures": "Categories,Description,Color",
+        	"details": "",
+        	"language": "en",
+        };
+
+        // Display the image.
+        var sourceImageUrl = "<?php echo $url ?>";
+        document.querySelector("#sourceImage").src = sourceImageUrl;
+
+        // Make the REST API call.
+        $.ajax({
+        	url: uriBase + "?" + $.param(params),
+
+            // Request headers.
+            beforeSend: function(xhrObj){
+            	xhrObj.setRequestHeader("Content-Type","application/json");
+            	xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+
+            type: "POST",
+
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+            // console.log(data);
+            // var json = $.parseJSON(data);
+            $("#description").text(data.description.captions[0].text);
+        })
+
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
+            errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+            jQuery.parseJSON(jqXHR.responseText).message;
+            alert(errorString);
+        });
+    });
 </script>
-    <?php
-        if (!empty($_GET['containerName'])) {
-    ?>
-        <script type="text/javascript">
-           $( "#result" ).show();
-                // **********************************************
-                // *** Update or verify the following values. ***
-                // **********************************************
-        
-                // Replace <Subscription Key> with your valid subscription key.
-                var subscriptionKey = "4c89c2f802374e608cc33c0c2e534972";
-        
-                // You must use the same Azure region in your REST API method as you used to
-                // get your subscription keys. For example, if you got your subscription keys
-                // from the West US region, replace "westcentralus" in the URL
-                // below with "westus".
-                //
-                // Free trial subscription keys are generated in the "westus" region.
-                // If you use a free trial subscription key, you shouldn't need to change
-                // this region.
-                var uriBase =
-                    "https://rachelvision.cognitiveservices.azure.com/";
-        
-                // Request parameters.
-                var params = {
-                    "visualFeatures": "Categories,Description,Color",
-                    "details": "",
-                    "language": "en",
-                };
-        
-                // Display the image.
-                var sourceImageUrl = document.getElementById("images").src;
-                // document.querySelector("#sourceImage").src = sourceImageUrl;
-        
-                // Make the REST API call.
-                $.ajax({
-                    url: uriBase + "?" + $.param(params),
-        
-                    // Request headers.
-                    beforeSend: function(xhrObj){
-                        xhrObj.setRequestHeader("Content-Type","application/json");
-                        xhrObj.setRequestHeader(
-                            "Ocp-Apim-Subscription-Key", subscriptionKey);
-                    },
-        
-                    type: "POST",
-        
-                    // Request body.
-                    data: '{"url": ' + '"' + sourceImageUrl + '"}',
-                })
-        
-                .done(function(data) {
-                    // Show formatted JSON on webpage.
-                    $("#captions").text(data['description']['captions'][0]['text']);
-                })
-        
-                .fail(function(jqXHR, textStatus, errorThrown) {
-                    // Display error message.
-                    var errorString = (errorThrown === "") ? "Error. " :
-                        errorThrown + " (" + jqXHR.status + "): ";
-                    errorString += (jqXHR.responseText === "") ? "" :
-                        jQuery.parseJSON(jqXHR.responseText).message;
-                    alert(errorString);
-                });
-        </script>
-    <?php
-        }
-    ?>
+
+<h1>Analyze image:</h1>
+<div id="wrapper" style="width:1020px; display:table;">
+	<div id="jsonOutput" style="width:600px; display:table-cell;">
+		Response:
+		<br><br>
+		<textarea id="responseTextArea" class="UIInput"
+		style="width:580px; height:400px;" readonly=""></textarea>
+	</div>
+	<div id="imageDiv" style="width:420px; display:table-cell;">
+		Source image:
+		<br><br>
+		<img id="sourceImage" width="400" />
+		<br>
+		<h3 id="description">Loading description. . .</h3>
+	</div>
+</div>
+</body>
 </html>
